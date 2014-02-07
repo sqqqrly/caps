1359139136cc8193824a017a672fe7c1aaf23411
dohlemacher <ohlemacher@ieee.org>
2014-02-07 02:56:19 -0800

    Fixed includes.

--- src/tools/get_sub.c
+++ src/tools/get_sub.c
@@ -4,6 +4,8 @@
  * extract subroutine  get_sub "name" <in >out
  */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 long to_end = 0;
 
@@ -17,7 +19,7 @@ char **argv;
   flag = 0;
   len = strlen(argv[1]);
   
-  if (argc > 2) to_end = 1;
+  if (argc > 2) { to_end = 1; }
 
   while(fgets(buf, 128, stdin) > 0)
   {
@@ -27,7 +29,7 @@ char **argv;
       if (buf[0] == '}' && !to_end) exit(0);
       continue;
     }
-    if (strlen(buf) < len) continue;
+    if (strlen(buf) < len) { continue; }
     if (strncmp(argv[1], buf, len) == 0) 
     {
       flag = 1;
@@ -37,7 +39,6 @@ char **argv;
   }
   exit(0);
 }
-
 
 /* end of get_sub.c */
 
