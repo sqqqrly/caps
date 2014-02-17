d63027ce7899e57348b122ec8c382cb5d3b7e92a
David Ohlemacher <ohlemacher@ieee.org>
2014-02-17 11:35:43 -0800

    Added hdrs

--- src/sys/ins_pm_screen.c
+++ src/sys/ins_pm_screen.c
@@ -23,6 +23,10 @@ static char ins_pm_screen_c[] = "%Z% %M% %I% (%G% - %U%)";
 /************************************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 #include "iodefs.h"
 #include "sd.h"
 #include "ss.h"
