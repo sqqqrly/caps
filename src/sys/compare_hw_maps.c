a83bd82e748ceeaa3dba608db18bf72d3368c7a8
David Ohlemacher <ohlemacher@ieee.org>
2014-02-17 09:07:41 -0800

    Added hdrs

--- src/sys/compare_hw_maps.c
+++ src/sys/compare_hw_maps.c
@@ -23,6 +23,10 @@
 static char compare_hw_maps_c[] = "%Z% %M% %I% (%G% - %U%)";
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 #include "file_names.h"
 #include "message_types.h"
 #include "ss.h"
