e075af4d1f97e963fcdf3b25e231655518d95d12
David Ohlemacher <ohlemacher@ieee.org>
2014-02-16 17:00:04 -0800

    Added needed hdrs

--- src/sys/backupmm.c
+++ src/sys/backupmm.c
@@ -26,6 +26,10 @@ static char backupmm_c[] = "%Z% %M% %I% (%G% - %U%)";
 /*                                                                          */
 /****************************************************************************/
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 #include "iodefs.h"
 #include "sd.h"
 #include "ss.h"
