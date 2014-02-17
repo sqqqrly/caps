27122e7c92cf7dab804c58cfcfe835ed34dd6f9f
David Ohlemacher <ohlemacher@ieee.org>
2014-02-17 10:15:28 -0800

    added hdrs

--- src/sys/database_report.c
+++ src/sys/database_report.c
@@ -59,6 +59,10 @@ static char database_report_c[] = "%Z% %M% %I% (%G% - %U%)";
 /*                                                                      */
 /************************************************************************/
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 #include "iodefs.h"
 #include "sd.h"
 #include "ss.h"
