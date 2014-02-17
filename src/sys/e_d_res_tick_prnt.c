8d2b3058706c96e645033609d670faa748690be7
David Ohlemacher <ohlemacher@ieee.org>
2014-02-17 11:21:23 -0800

    Added hdrs

--- src/sys/e_d_res_tick_prnt.c
+++ src/sys/e_d_res_tick_prnt.c
@@ -21,6 +21,10 @@ static char e_d_res_tick_prnt_c[] = "%Z% %M% %I% (%G% - %U%)";
 /*                                                                      */
 /************************************************************************/
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 #include "iodefs.h"
 #include "sd.h"
 #include "ss.h"
