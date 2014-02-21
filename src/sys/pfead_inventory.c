81d31367ea7368c4ed4c163b9e239173a98826bc
David Ohlemacher <ohlemacher@ieee.org>
2014-02-21 15:47:29 -0800

    Added hdrs

--- src/sys/pfead_inventory.c
+++ src/sys/pfead_inventory.c
@@ -23,6 +23,10 @@ static char pfead_inventory_c[] = "%Z% %M% %I% (%G% - %U%)";
 /************************************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 #include "iodefs.h"
 #include "getparms.h"
 #include "sd.h"
