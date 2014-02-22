e6b4ef7d5384548326c33f40dc1c13322e18dcb0
David Ohlemacher <ohlemacher@ieee.org>
2014-02-21 21:15:35 -0800

    Added hdrs

--- src/sys/stock_stat_create.c
+++ src/sys/stock_stat_create.c
@@ -36,6 +36,10 @@ static char stock_stat_create_c[] = "%Z% %M% %I% (%G% - %U%)";
 /************************************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 #include "iodefs.h"
 #include "ss.h"
 #include "co.h"
