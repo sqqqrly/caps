2c1461f2d8d2badd6eae8b76fc4b9b905d485267
David Ohlemacher <ohlemacher@ieee.org>
2014-02-21 17:20:44 -0800

    Added hdrs

--- src/sys/picker_login_rpt.c
+++ src/sys/picker_login_rpt.c
@@ -19,6 +19,10 @@ static char picker_login_rpt_c[] = "%Z% %M% %I% (%G% - %U%)";
 #include "caps_copyright.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 #include "iodefs.h"
 #include "sd.h"
 #include "ss.h"
