c701e11da8b95fa3174b5a60c4348ccaacfc9b8e
David Ohlemacher <ohlemacher@ieee.org>
2014-02-22 06:27:41 -0800

    Added hdrs

--- src/sys/view_menu.c
+++ src/sys/view_menu.c
@@ -17,6 +17,10 @@
 static char view_menu_c[] = "%Z% %M% %I% (%G% - %U%)";
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 #include "iodefs.h"
 #include "ss.h"
 #include "sd.h"
