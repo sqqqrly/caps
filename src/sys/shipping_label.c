d100ccb9dc9d89892f1a6b44561fe3b2828785a1
David Ohlemacher <ohlemacher@ieee.org>
2014-02-21 21:07:26 -0800

    Added hdrs. Fixed leave() signal handler. This file includes remarks_text.txt, shipping_defines.h and shipping_label.h. There are copies of the files in src/h and src/h/custom/labels.

--- src/sys/shipping_label.c
+++ src/sys/shipping_label.c
@@ -35,6 +35,9 @@ static char shipping_label_c[] = "%Z% %M% %I% (%G% - %U%)";
  *                             -value=x  indicates 'x' as alpha fill byte
  *-------------------------------------------------------------------------*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include <signal.h>
 #include "file_names.h"
 #include "ss.h"
@@ -138,7 +141,7 @@ main(argc, argv)
 long  argc;
 char  **argv;
 {
-  extern leave();
+  void leave(int signum); // Signal handler
   register long k;
   register char *p;
 
@@ -214,7 +217,7 @@ char  **argv;
   image = (char *)malloc(LENGTH * WIDTH); /* print page work image           */
   memset(image, 0x20, WIDTH * LENGTH);    /* clear image initially           */
    
-  if (debug) {do_debug(); leave();}
+  if (debug) {do_debug(); leave(0);}
 
   while (1)
   {
@@ -263,7 +266,7 @@ char  **argv;
 /*------------------------------------------------------------------------*
  *  Terminate On Shutdown
  *------------------------------------------------------------------------*/
-leave()
+void leave(int signum)
 {
   printf("\rShipping Labels Stopping On Shutdown\n");
   ss_close();
