f9058f0bfb969085ba48e89cb0332ef0dc944564
David Ohlemacher <ohlemacher@ieee.org>
2014-02-22 04:38:26 -0800

    Added hdrs

--- src/sys/test_menu.c
+++ src/sys/test_menu.c
@@ -21,6 +21,10 @@
 static char test_menu_c[] = "%Z% %M% %I% (%G% - %U%)";
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 #include "iodefs.h"
 #include "sd.h"
 #include "ss.h"
